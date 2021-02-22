2538552cbd9baa13580baddada241eb0ba5cc5e2
imthelin <243404855@qq.com>
2021-02-22 16:08:27 +0800

    feat: 增加 eslint\prettier

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+/*
+ * @Description: 
+ * @Author: GangLin.Chen
+ * @Date: 2021-02-22 16:07:19
+ * @LastEditors: OBKoro1
+ * @LastEditTime: 2021-02-22 16:07:20
+ * @FilePath: /vite-vue/.prettierrc.js
+ * @filePathColon: \\
+ */
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  endOfLine:"auto"
+};
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/index.ts'
-import store from './store/index.ts'
+import router from './router/index'
+import store from './store/index'
 import 'vant/lib/index.css'
 import './utils/rem.ts'
 
 createApp(App)
   .use(router)
   .use(store)
-  // .use(vant)
   .mount('#app')
