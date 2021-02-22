f669fd7a31101cbcc571b947d8e90d19532c6de3
imthelin <243404855@qq.com>
2021-02-22 16:10:32 +0800

    feat: 优化编译提示，声明静态文件

---
+++ src/source.d.ts
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: GangLin.Chen
+ * @Date: 2021-02-22 16:09:12
+ * @LastEditors: OBKoro1
+ * @LastEditTime: 2021-02-22 16:10:08
+ * @FilePath: /vite-vue/src/source.d.ts
+ * @filePathColon: \\
+ */
+declare module '*.json';
+declare module '*.png';
+declare module '*.jpg';
